4ea34788dab8520a180aacf14a82e95c8db354b6
SlyPrecious <slypreciousmutare1@gmail.com>
2025-02-10 08:43:54 +0200

    Update slyretail.js

--- slyretail.js
+++ slyretail.js
@@ -870,6 +870,6 @@ app.delete('/deletePaymentTypeRows', async (req, res) => {
 //   console.log("Server started on port 2000");
 // });
 
-app.listen(80, function () {
-  console.log("Server started on port 80");
+app.listen(2000, function () {
+  console.log("Server started on port 2000");
 });
