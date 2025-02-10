f1d6125c7e207c86764f03146ebf88a9c85b8c18
SlyPrecious <slypreciousmutare1@gmail.com>
2025-02-10 15:21:35 +0200

    Update accountingPeriodController.js

--- Controllers/accountingPeriodController.js
+++ Controllers/accountingPeriodController.js
@@ -1,38 +1,53 @@
 import { accountingPeriodModel } from '../Schemas/slyretailAccountingPeriodSettingsSchemas.js';
 import { ObjectId } from 'mongodb';
+import { connectDB, databaseName } from '../Schemas/slyretailDbConfig.js';
 
-let isModified=false
+let isModified = false
+
 export async function getAccountingPeriodDetails() {
-        
-  try {
-    const details = await accountingPeriodModel.find();
-    return { details };
-  } catch (err) {
-    console.error('Error fetching accounting details:', err);
-    res.status(500).json({ error: 'Internal server error' });
+  // Step 1: Create a connection
+  const db = await connectDB(databaseName);
+  if (db) {
+    try {
+      // const accountingPeriodModel = db.model('Accountingperiod', AccountingPeriodSettingsSchema);
+      // Create the model with the specific connection
+      const accountingModel = accountingPeriodModel(db);
+      const details = await accountingModel.find();
+
+      return { details };
+    } catch (err) {
+      console.error('Error fetching accounting details:', err);
+      res.status(500).json({ error: 'Internal server error' });
+    }
   }
 }
-export async function updateAccountingPeriod(id,startDate) {
-    // Parse the startDate to a Date object
-  try{  const start = new Date(startDate);
-    // Calculate end date as December 31 of the same year
-    // const endDate = new Date(start.getFullYear(), 11, 31); // Month is 0-indexed
+export async function updateAccountingPeriod(id, startDate) {
+  // Parse the startDate to a Date object
+  try {
+    // Step 1: Create a connection
+    const db = await connectDB(databaseName);
+    if (db) {
+      const start = new Date(startDate);
+      // Calculate end date as December 31 of the same year
+      // const endDate = new Date(start.getFullYear(), 11, 31); // Month is 0-indexed
+      const accountingModel = accountingPeriodModel(db);
+      const result = await accountingModel.updateOne(
+        // const result = await accountingPeriodModel.updateOne(
+        { _id: ObjectId(id) },
+        { $set: { startDate: start } }
+      );
 
-    const result = await accountingPeriodModel.updateOne(
-      { _id: ObjectId(id) },
-      { $set: { startDate: start } }
-    );
+      console.log("Accounting period settings updated successfully:", result.modifiedCount);
 
-    console.log("Accounting period settings updated successfully:", result.modifiedCount);
-
-    if (result.modifiedCount > 0) {
-      isModified = true;
-    } else {
-      isModified = false;
-    }  
-    return isModified;
-}
-catch (err) {
+      if (result.modifiedCount > 0) {
+        isModified = true;
+      } else {
+        isModified = false;
+      }
+      return isModified;
+    }
+  }
+  catch (err) {
     console.error('Error updating accounting details:', err);
     res.status(500).json({ error: 'Internal server error' });
   }
