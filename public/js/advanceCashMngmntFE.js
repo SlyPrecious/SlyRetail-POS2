d17ee0a1c04df0e88bee535114454680cd0a1e92
SlyPrecious <slypreciousmutare1@gmail.com>
2025-02-11 13:45:41 +0200

    Update advanceCashMngmntFE.js

--- public/js/advanceCashMngmntFE.js
+++ public/js/advanceCashMngmntFE.js
@@ -3174,8 +3174,8 @@ return
 
                                     if (rowId === '') {
                                         // MOVE FOCUS TO DESCRIPTION CELL
-                                        descriptionCell.focus();
                                         descriptionCell.contenteditable=true
+                                        descriptionCell.focus();
                                     }
                                     else if (rowId !== '') {
                                         //  use the fetch for the route with POST method and update the expense rate in the database
