7a1ef77b6c22b1da7ba8272cb7d6c25bc982ab87
SlyPrecious <slypreciousmutare1@gmail.com>
2025-02-12 17:04:09 +0200

    Update loginPageController.js

--- Controllers/loginPageController.js
+++ Controllers/loginPageController.js
@@ -27,6 +27,7 @@ async function signUpSignIn(req,databaseName, email, databasePassword, signingCr
         // console.log('myDatabase'+myDatabase)
         // console.log('signCriteria'+signCriteria)
         if (connection) {
+            console.log('connection')
            // Store the connection and other session data (this should be done in connectDB itself)
             // req.session.connection = connection.connectionString;  // Session is already populated with the connection and other details
                // Save models in session if you need to use them later
