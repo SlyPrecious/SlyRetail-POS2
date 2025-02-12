3a4b0b2f10a32e4fe033d8426d7ecb4ea2749450
SlyPrecious <slypreciousmutare1@gmail.com>
2025-02-12 12:07:11 +0200

    Update currenciesController.js

--- Controllers/currenciesController.js
+++ Controllers/currenciesController.js
@@ -1,21 +1,21 @@
 import { CurrenciesModel } from '../Schemas/slyretailCurrenciesSchemas.js';
 import { ObjectId } from 'mongodb';
-import { connectDB, myDatabase,signCriteria } from '../Schemas/slyretailDbConfig.js';
 
 let isUpdated = false
 let amDeleted = false
 let isSaved = false;
 let insertedId = ''
 
-export async function updateCurrencies(currencyId, paymentType, paymentName, paymentRate) {
+export async function updateCurrencies(req,currencyId, paymentType, paymentName, paymentRate) {
     try {
-              const db = await connectDB(myDatabase,signCriteria);
-  if (db) {
-     const  myCurrenciesModelModel = CurrenciesModel(db);
-    const cashFlowCat = await myCurrenciesModelModel.find();
+const { models } = req.session; //get the models in the session storage
+if (models) {
+    // Access the models from the session
+const {currenciesModel} = models;
+    const cashFlowCat = await currenciesModel.find();
         console.log('i am the  procedure of updating  currencies document in payment type ');
         // insert the new payment
-        await myCurrenciesModelModel.updateOne({ _id: ObjectId(currencyId) }, {
+        await currenciesModel.updateOne({ _id: ObjectId(currencyId) }, {
             $set: {
                 Currency_Name: paymentName,
                 RATE: Number(paymentRate),
@@ -38,15 +38,16 @@ export async function updateCurrencies(currencyId, paymentType, paymentName, pay
     return { isUpdated };
 }
 //===========================================================================================
-export async function updateCurrencyName(currencyId, paymentType) {
+export async function updateCurrencyName(req,currencyId, paymentType) {
     try {
-            const db = await connectDB(myDatabase,signCriteria);
-  if (db) {
-     const  myCurrenciesModelModel = CurrenciesModel(db);
-    // const cashFlowCat = await myCurrenciesModelModel.find();
+         const { models } = req.session; //get the models in the session storage
+if (models) {
+    // Access the models from the session
+const {currenciesModel} = models;
+    // const cashFlowCat = await currenciesModel.find();
         console.log('i am the  procedure of updating  currencies name in payment type ');
         // insert the new payment
-        await myCurrenciesModelModel.updateOne({ _id: ObjectId(currencyId) }, {
+        await currenciesModel.updateOne({ _id: ObjectId(currencyId) }, {
             $set: {
                 paymentType: paymentType
             }
@@ -59,25 +60,27 @@ export async function updateCurrencyName(currencyId, paymentType) {
                 }
             })
             .catch(error => console.error(error));
-  }
+     return { isUpdated }; 
+}
     }
     catch (err) {
         console.error('Error UPDATING CURRENCIES:', err);
     }
-    return { isUpdated };
+
 }
 //=========================================================================================================================
-export async function insertNewCurrency(paymentType, paymentName, paymentRate) {
+export async function insertNewCurrency(req,paymentType, paymentName, paymentRate) {
     try {
         console.log('procedure for inserting new currency' + paymentType, paymentName, paymentRate)
-         const db = await connectDB(myDatabase,signCriteria);
-  if (db) {
-     const  myCurrenciesModelModel = CurrenciesModel(db); // insert the new payment
+      const { models } = req.session; //get the models in the session storage
+if (models) {
+    // Access the models from the session
+const {currenciesModel} = models;
         let data = {
             Currency_Name: paymentName, BASE_CURRENCY: " N", //TO FIX THIS WE NEED AN N/OFF BUTTON FOR BASE CURRENCY OPTION
             RATE: Number(paymentRate), paymentType: paymentType
         }
-        const currencyEntry = new myCurrenciesModelModel(data);
+        const currencyEntry = new currenciesModel(data);
         try {
             const result = await currencyEntry.save();
             if (result) {
@@ -98,19 +101,20 @@ export async function insertNewCurrency(paymentType, paymentName, paymentRate) {
 }
 
 //==========================================================================================================================
-export async function updateBaseCurrency(paymentId) {
+export async function updateBaseCurrency(req,paymentId) {
     try {
         console.log('i am the update base currency procedure');
-         const db = await connectDB(myDatabase,signCriteria);
-     if (db) {
-     const  myCurrenciesModelModel = CurrenciesModel(db); // insert the new payment
-        const currencies = await myCurrenciesModelModel.find(); //Put the multiple currencies into an array 'note that this is the whole document, but we want to tap into the name object only'
+       const { models } = req.session; //get the models in the session storage
+if (models) {
+    // Access the models from the session
+const {currenciesModel} = models;
+        const currencies = await currenciesModel.find(); //Put the multiple currencies into an array 'note that this is the whole document, but we want to tap into the name object only'
         // Loop through the currencies and check for the ID of the checked one
         for (let i = 0; i < currencies.length; i++) {
             const currency = currencies[i];
             const isBaseCurrency = currency._id.toString() === paymentId;
             //FIRST CONVERT ALL CURRENCIES TO N
-            await myCurrenciesModelModel.updateOne({ _id: ObjectId(currency._id) }, { $set: { BASE_CURRENCY: 'N' } })
+            await currenciesModel.updateOne({ _id: ObjectId(currency._id) }, { $set: { BASE_CURRENCY: 'N' } })
                 .then(result => {
                     console.log(`Updated ${result.modifiedCount} documents to N.`);
                 })
@@ -119,7 +123,7 @@ export async function updateBaseCurrency(paymentId) {
                 });
             // If it's the checked currency, update the baseCurrency field to "Y"
             if (isBaseCurrency) {
-                await myCurrenciesModelModel.updateOne({ _id: ObjectId(paymentId) }, { $set: { BASE_CURRENCY: 'Y' } })
+                await currenciesModel.updateOne({ _id: ObjectId(paymentId) }, { $set: { BASE_CURRENCY: 'Y' } })
                     .then(result => {
                         console.log(`Updated ${result.modifiedCount} documents to Y`);
                         if (result.modifiedCount !== 0) {
@@ -146,14 +150,14 @@ export async function updateBaseCurrency(paymentId) {
     return { isUpdated };
 }
 //==========================================================================================================
-export async function updateCurrencyRate(currencyId, CurrencyRate) {
+export async function updateCurrencyRate(req,currencyId, CurrencyRate) {
     try {
         console.log('i am the  procedure of updating  currencies rate in payment type ');
-        // insert the new payment
-         const db = await connectDB(myDatabase,signCriteria);
-     if (db) {
-     const  myCurrenciesModelModel = CurrenciesModel(db); 
-        await myCurrenciesModelModel.updateOne({ _id: ObjectId(currencyId) }, { $set: { RATE: Number(CurrencyRate) } })
+      const { models } = req.session; //get the models in the session storage
+if (models) {
+    // Access the models from the session
+const {currenciesModel} = models;
+        await currenciesModel.updateOne({ _id: ObjectId(currencyId) }, { $set: { RATE: Number(CurrencyRate) } })
             .then(result => {
                 console.log(`${result.matchedCount} document(s) matched the filter criteria.`);
                 console.log(`${result.modifiedCount} document(s) were updated with the new field value.`);
@@ -170,16 +174,17 @@ export async function updateCurrencyRate(currencyId, CurrencyRate) {
     return { isUpdated };
 }
 //==========================================================================================================
-export async function deleteCurrency(idToDelete) {
+export async function deleteCurrency(req,idToDelete) {
     try {
         console.log('i am the  procedure of deleting currency  ');
-      const db = await connectDB(myDatabase,signCriteria);
-     if (db) {
-     const  myCurrenciesModelModel = CurrenciesModel(db); 
+    const { models } = req.session; //get the models in the session storage
+if (models) {
+    // Access the models from the session
+const {currenciesModel,cashflowModal} = models;
         for (let a = 0; a < idToDelete.length; a++) {
             const element = idToDelete[a];
             // insert the new payment
-            await myCurrenciesModelModel.deleteOne({ _id: ObjectId(element) })
+            await currenciesModel.deleteOne({ _id: ObjectId(element) })
                 .then(result => {
                     console.log(`${result.deletedCount} document(s) matched the filter criteria.`);
                     if (result.deletedCount !== 0) {
