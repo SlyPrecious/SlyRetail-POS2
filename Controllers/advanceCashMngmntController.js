4759dabdd1e09146c6c7e3627ad3a3454fb0e1c4
SlyPrecious <slypreciousmutare1@gmail.com>
2025-02-18 16:22:32 +0200

    Update advanceCashMngmntController.js

--- Controllers/advanceCashMngmntController.js
+++ Controllers/advanceCashMngmntController.js
@@ -1,42 +1,38 @@
-import { dbConnection } from "../Controllers/loginPageController.js";
-import { ObjectId } from 'mongodb';
 import { CurrenciesModel } from '../Schemas/slyretailCurrenciesSchemas.js';
-import { ExpenseCategoriesModel } from '../Schemas/slyretailExpenseCategoriesSchemas.js';
-import { IncomeCategoriesModel } from '../Schemas/slyretailIncomeCategoriesSchemas.js';
+import { connectDB } from '../Schemas/slyretailDbConfig.js';
 import { WorldCurrencies } from "../public/js/worldCurrency.js";
 
 let currencies = [];
-let intervalArray = [];
 let isBaseCurrency = "";
 let isoCode = '';
-let symbols = {};//this variable object will contain all the currency symbolss in the expense table
-export async function advCashMngmnt(req) {
+let databaseName = ""
+let signingCriteria = ""
+export async function advCashMngmnt(req, sessionId) {
 
     try {
-         const { models } = req.session; //get the models in the session storage
-       if (!models) {
-      throw new Error('Session models not found');
-    }
-             // Access the models from the session
-const { credentialsModel,advHeadersModel, cashflowModel,versionControlModel, currenciesModel,accountingPeriodModel} = models;
-        // //THIS CODE IS SENDING THE ARRAY OF CURRENCIES FROM THE DATABASE TO THE HTML/ CLIENT'S SIDE THE LIST OF CURRENCIES ON THE MY EXPENSES DROPDOWN MENU
-        currencies = await currenciesModel.find()
-        //find the base currency in the collection that is where there is a Y
-        const baseCurrency = await currenciesModel.findOne({ BASE_CURRENCY: 'Y' });
-        //LOOP WITHIN THE WORLD CURRENCIES ARRAY SO THAT WE CAN ACCESS THE ISO CODE FOR THE BASE CURRENCY SELECTED
-        for (let i = 0; i < WorldCurrencies.length; i++) {
-            const WorldCurrency = WorldCurrencies[i];
-            if ((WorldCurrency.Currency_Name).toLowerCase() === (baseCurrency.Currency_Name).toLowerCase()) {
-                isoCode = WorldCurrency.ISO_Code;
+        const db = await connectDB(req, databaseName, signingCriteria, sessionId);
+        if (db) {
+            // Create the model with the specific connection
+            const mycurrenciesModel = CurrenciesModel(db);
+            // //THIS CODE IS SENDING THE ARRAY OF CURRENCIES FROM THE DATABASE TO THE HTML/ CLIENT'S SIDE THE LIST OF CURRENCIES ON THE MY EXPENSES DROPDOWN MENU
+            currencies = await mycurrenciesModel.find()
+            //find the base currency in the collection that is where there is a Y
+            const baseCurrency = await mycurrenciesModel.findOne({ BASE_CURRENCY: 'Y' });
+            //LOOP WITHIN THE WORLD CURRENCIES ARRAY SO THAT WE CAN ACCESS THE ISO CODE FOR THE BASE CURRENCY SELECTED
+            for (let i = 0; i < WorldCurrencies.length; i++) {
+                const WorldCurrency = WorldCurrencies[i];
+                if ((WorldCurrency.Currency_Name).toLowerCase() === (baseCurrency.Currency_Name).toLowerCase()) {
+                    isoCode = WorldCurrency.ISO_Code;
+                }
             }
+            // now take the  name of the base currency  and store it in a variable
+            isBaseCurrency = baseCurrency.Currency_Name;
+            //CREATE THE INTERVAL ARRAY
         }
-        // now take the  name of the base currency  and store it in a variable 
-        isBaseCurrency = baseCurrency.Currency_Name;
-        //CREATE THE INTERVAL ARRAY
-    return { isBaseCurrency: isBaseCurrency,  currencies: currencies, isoCode: isoCode };
-         
-    }catch (err) {
+    }
+    catch (err) {
         console.error('Error connecting to MongoDB:', err);
     }
+    return { isBaseCurrency: isBaseCurrency, currencies: currencies, isoCode: isoCode };
 }
 
