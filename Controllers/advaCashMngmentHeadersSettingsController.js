04273820421fb322575d4db74c4b308f6c473dc3
SlyPrecious <slypreciousmutare1@gmail.com>
2025-02-18 16:22:05 +0200

    Update advaCashMngmentHeadersSettingsController.js

--- Controllers/advaCashMngmentHeadersSettingsController.js
+++ Controllers/advaCashMngmentHeadersSettingsController.js
@@ -1,47 +1,51 @@
 import { advaHeadersModel } from '../Schemas/slyretailAdvCashMngmntHeadersSettingsSchemas.js';
+import { connectDB } from '../Schemas/slyretailDbConfig.js';
 let advancedHeaderStatus = []
 let isSaving = false;
 let modifiedCount = ""
-export async function getadvancedHeaderStatusArray(req) {
-    try {
-      const { models } = req.session; //get the models in the session storage
-                 // Access the models from the session
-       if (!models) {
-      throw new Error('Session models not found');
+let databaseName = ""
+let signingCriteria = ""
+
+export async function getadvancedHeaderStatusArray(req, sessionId) {
+    const db = await connectDB(req, databaseName, signingCriteria, sessionId);
+    if (db) {
+        // Create the model with the specific connection
+        const myaccountingPeriodModelModel = advaHeadersModel(db);
+        try {
+            advancedHeaderStatus = await myaccountingPeriodModelModel.find()
+            return { advancedHeaderStatus };
+        } catch (err) {
+            console.error('Error fetching status:', err);
+        }
     }
-        const {advHeadersModel} = models;
-      const advancedHeaderStatus = await advHeadersModel.find();
-        return { advancedHeaderStatus };
-      
-    } catch (err) {
-        console.error('Error fetching status:', err);
-    }
+
 }
-export async function saveHeaderStatusAdv(req,headerNamefcb, headerisDisplayed) {
-        //THERE ARE OTHER HEADERS LIKE VAT THAT SHOULD BE OPENED AFTER SUBSCRIPTIONS, ALL THOSE LOGIC WILL BE MANAGED HERE
-         try {
-      const { models } = req.session; //get the models in the session storage
-                 // Access the models from the session
-      if (!models) {
-      throw new Error('Session models not found');
-    }
-        const {advHeadersModel} = models;
-            await advHeadersModel.updateOne({ HeaderName: headerNamefcb }, {
-            $set: {
-                isDisplayed: headerisDisplayed
-            }
-        }).then(result => {
-            console.log(`${result.modifiedCount} document(s) updated.`);
-            modifiedCount = result.modifiedCount
-            if ( modifiedCount !== 0) {
-                isSaving = true;
-            }
-           else if ( modifiedCount === 0) {
-                isSaving = false;
-            }
-        })
-        return { isSaving };   
-    }catch (error) {
-        console.error(error)
+export async function saveHeaderStatusAdv(req, headerNamefcb, headerisDisplayed, sessionId) {
+    // process the database connection request
+    const db = await connectDB(req, databaseName, signingCriteria, sessionId);
+    if (db) {
+        // Create the model with the specific connection
+        const myaccountingPeriodModelModel = advaHeadersModel(db);
+        try {
+            //THERE ARE OTHER HEADERS LIKE VAT THAT SHOULD BE OPENED AFTER SUBSCRIPTIONS, ALL THOSE LOGIC WILL BE MANAGED HERE
+            await myaccountingPeriodModelModel.updateOne({ HeaderName: headerNamefcb }, {
+                $set: {
+                    isDisplayed: headerisDisplayed
+                }
+            }).then(result => {
+                console.log(`${result.modifiedCount} document(s) updated.`);
+                modifiedCount = result.modifiedCount
+                if (modifiedCount !== 0) {
+                    isSaving = true;
+                }
+                else if (modifiedCount === 0) {
+                    isSaving = false;
+                }
+            })
+            return { isSaving };
+        }
+        catch (error) {
+            console.error(error)
+        }
     }
 }
