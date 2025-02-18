46e0378e72018511077687d8f5e3958f8bbc2f2d
SlyPrecious <slypreciousmutare1@gmail.com>
2025-02-18 16:21:49 +0200

    Update accountingPeriodController.js

--- Controllers/accountingPeriodController.js
+++ Controllers/accountingPeriodController.js
@@ -1,43 +1,40 @@
 import { accountingPeriodModel } from '../Schemas/slyretailAccountingPeriodSettingsSchemas.js';
 import { ObjectId } from 'mongodb';
-// import { connectDB, myDatabase,signCriteria} from '../Schemas/slyretailDbConfig.js';
+import { connectDB } from '../Schemas/slyretailDbConfig.js';
 
 let isModified = false
+let databaseName = ""
+let signingCriteria = ""
 
-export async function getAccountingPeriodDetails(req) {
-  const { models } = req.session; //get the models in the session storage
- 
+export async function getAccountingPeriodDetails(req, sessionId) {
+  // Step 1: Create a connection
+  const db = await connectDB(req, databaseName, signingCriteria, sessionId);
+  if (db) {
     try {
-    if (!models) {
-      throw new Error('Session models not found');
-    }
-        // Access the models from the session
-        const { accountingPeriodModel} = models;
       // const accountingPeriodModel = db.model('Accountingperiod', AccountingPeriodSettingsSchema);
       // Create the model with the specific connection
-      const details = await accountingPeriodModel.find();
+      const accountingModel = accountingPeriodModel(db);
+      const details = await accountingModel.find();
 
-      return { details };     
+      return { details };
     } catch (err) {
       console.error('Error fetching accounting details:', err);
       res.status(500).json({ error: 'Internal server error' });
+    }
   }
 }
-export async function updateAccountingPeriod(req,id, startDate) {
+//============================================================================================================
+export async function updateAccountingPeriod(req, id, startDate, sessionId) {
   // Parse the startDate to a Date object
   try {
     // Step 1: Create a connection
-  const { models } = req.session; //get the models in the session storage
- if (!models) {
-      throw new Error('Session models not found');
-    }
-        // Access the models from the session
-        const {accountingPeriodModel} = models;
+    const db = await connectDB(req, databaseName, signingCriteria, sessionId);
+    if (db) {
       const start = new Date(startDate);
       // Calculate end date as December 31 of the same year
       // const endDate = new Date(start.getFullYear(), 11, 31); // Month is 0-indexed
-   
-      const result = await accountingPeriodModel.updateOne(
+      const accountingModel = accountingPeriodModel(db);
+      const result = await accountingModel.updateOne(
         // const result = await accountingPeriodModel.updateOne(
         { _id: ObjectId(id) },
         { $set: { startDate: start } }
@@ -51,8 +48,9 @@ export async function updateAccountingPeriod(req,id, startDate) {
         isModified = false;
       }
       return isModified;
-    
-  }catch (err) {
+    }
+  }
+  catch (err) {
     console.error('Error updating accounting details:', err);
     res.status(500).json({ error: 'Internal server error' });
   }
